69672441f3c7dd2adde4132042de95d2c8383d18
Samuel Oliveira <br.samuel.oliveira@gmail.com>
2020-09-10 12:03:20 -0400

    Add melhorias visuais

--- app/script.js
+++ app/script.js
@@ -1,6 +1,8 @@
+let inputCounter = 0;
 let todosUl = document.querySelector(".todos ul");
 let todos = document.querySelectorAll(".todos li");
 let todoInput = document.querySelector(".todos input");
+let addTodoButton = document.querySelector(".header img");
 let deleteButton = document.querySelectorAll(".todos .deleteButton");
 
 init();
@@ -8,6 +10,7 @@ init();
 function init() {
     mouseEvents();
     deleteEvents();
+    addTodoEvent();
     inputEvents();
 }
 
@@ -23,8 +26,7 @@ function mouseEvents() {
         });
         
         todos[i].addEventListener("click", function(){
-            this.style.color = "#44445B";
-            this.style.textDecoration = "line-through";
+            this.classList.toggle("todo-done");
         });
     }
 }
@@ -37,13 +39,20 @@ function deleteEvents() {
     }
 }
 
+function addTodoEvent() {
+    addTodoButton.addEventListener("click", function(){
+        todoInput.classList.toggle("display-input");
+    });
+}
+
 function inputEvents() {
     todoInput.addEventListener("keyup", function(event){
-        if(event.keyCode === 13) {
+        if(event.keyCode === 13 && inputCounter < 21) {
             //Receive Todo
             let text = todoInput.value;
             let li = "<li><span class='deleteButton'>X</span>"+ text + "</li>"; 
             todoInput.value = "";
+            inputCounter++;
             
             //Updating Todo List
             todosUl.insertAdjacentHTML('beforeend', li);
