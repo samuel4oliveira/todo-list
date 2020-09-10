15c02a21e3e3948b45a0af8d01a97d5a48fcee9d
Samuel Oliveira <br.samuel.oliveira@gmail.com>
2020-09-10 19:11:37 -0400

    Bugfix: Add opções Done e Delete para novos Todos

--- app/script.js
+++ app/script.js
@@ -1,22 +1,22 @@
-let inputCounter = 0;
-let todosUl = document.querySelector(".todos ul");
+let todosUl = document.querySelector("#todosUl");
 let todos = document.querySelectorAll(".todos li");
 let todoInput = document.querySelector(".todos input");
 let addTodoButton = document.querySelector(".header img");
 let deleteButton = document.querySelectorAll(".todos .deleteButton");
+let todosCounter = document.querySelectorAll("li:not(.delete-todo)").length;
 
 init();
 
 function init() {
     mouseEvents();
+    clickEvents();
     deleteEvents();
-    addTodoEvent();
+    addEvents();
     inputEvents();
 }
 
 function mouseEvents() {
     for(let i = 0; i < todos.length; i++) {
-    
         todos[i].addEventListener("mouseover", function(){
             deleteButton[i].style.display = "inline";
         });
@@ -24,22 +24,27 @@ function mouseEvents() {
         todos[i].addEventListener("mouseout", function(){
             deleteButton[i].style.display = "none";
         });
-        
-        todos[i].addEventListener("click", function(){
-            this.classList.toggle("todo-done");
-        });
     }
+}
+
+function clickEvents() {
+    todosUl.addEventListener("click", function(event){
+        const { target } = event;
+        if (target.tagName !== 'LI') return;
+        target.classList.toggle("todo-done");
+    });
 }
 
 function deleteEvents() {
     for(let i = 0; i < deleteButton.length; i++) {
         deleteButton[i].addEventListener("click", function(){
-            todos[i].style.display = "none";
+            todos[i].classList.add("delete-todo");
+            todosCounter = document.querySelectorAll("li:not(.delete-todo)").length;
         });
     }
 }
 
-function addTodoEvent() {
+function addEvents() {
     addTodoButton.addEventListener("click", function(){
         todoInput.classList.toggle("display-input");
     });
@@ -47,17 +52,17 @@ function addTodoEvent() {
 
 function inputEvents() {
     todoInput.addEventListener("keyup", function(event){
-        if(event.keyCode === 13 && inputCounter < 21) {
+        if(event.keyCode === 13 && todosCounter < 13) {
             //Receive Todo
             let text = todoInput.value;
             let li = "<li><span class='deleteButton'>X</span>"+ text + "</li>"; 
             todoInput.value = "";
-            inputCounter++;
             
-            //Updating Todo List
+            //Updating Todo List Variables
             todosUl.insertAdjacentHTML('beforeend', li);
             todos = document.querySelectorAll(".todos li");
             deleteButton = document.querySelectorAll(".deleteButton");
+            todosCounter = document.querySelectorAll("li:not(.delete-todo)").length;
             mouseEvents();
             deleteEvents();
         }
