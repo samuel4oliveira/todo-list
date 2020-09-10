5250743124d4a56673a152aae30283be776c2d63
Samuel Oliveira <br.samuel.oliveira@gmail.com>
2020-09-09 22:02:45 -0400

    Reestruturação de código e BugFixes

--- app/script.js
+++ app/script.js
@@ -1,32 +1,56 @@
 let todosUl = document.querySelector(".todos ul");
 let todos = document.querySelectorAll(".todos li");
 let todoInput = document.querySelector(".todos input");
-let deleteButton = document.querySelectorAll(".deleteButton");
+let deleteButton = document.querySelectorAll(".todos .deleteButton");
 
-for(let i = 0; i < todos.length; i++) {
-    todos[i].addEventListener("mouseover", function(){
-        deleteButton[i].style.display = "inline";
-    });
+init();
+
+function init() {
+    mouseEvents();
+    deleteEvents();
+    inputEvents();
+}
+
+function mouseEvents() {
+    for(let i = 0; i < todos.length; i++) {
     
-    todos[i].addEventListener("mouseout", function(){
-        deleteButton[i].style.display = "none";
-    });
-
-    todos[i].addEventListener("click", function(){
-        this.style.color = "#44445B";
-        this.style.textDecoration = "line-through";
-    });
+        todos[i].addEventListener("mouseover", function(){
+            deleteButton[i].style.display = "inline";
+        });
+        
+        todos[i].addEventListener("mouseout", function(){
+            deleteButton[i].style.display = "none";
+        });
+        
+        todos[i].addEventListener("click", function(){
+            this.style.color = "#44445B";
+            this.style.textDecoration = "line-through";
+        });
+    }
 }
 
-for(let i = 0; i < deleteButton.length; i++) {
-    deleteButton[i].addEventListener("click", function(){
-        todos[i].style.display = "none";
-    });
+function deleteEvents() {
+    for(let i = 0; i < deleteButton.length; i++) {
+        deleteButton[i].addEventListener("click", function(){
+            todos[i].style.display = "none";
+        });
+    }
 }
 
-todoInput.addEventListener("click", function(){
-    let text = todoInput.value;
-    let li = "<li>"+ text + "</li>"; 
-    todosUl.insertAdjacentHTML('beforeend', li);
-    todoInput.value = "";
-});
+function inputEvents() {
+    todoInput.addEventListener("keyup", function(event){
+        if(event.keyCode === 13) {
+            //Receive Todo
+            let text = todoInput.value;
+            let li = "<li><span class='deleteButton'>X</span>"+ text + "</li>"; 
+            todoInput.value = "";
+            
+            //Updating Todo List
+            todosUl.insertAdjacentHTML('beforeend', li);
+            todos = document.querySelectorAll(".todos li");
+            deleteButton = document.querySelectorAll(".deleteButton");
+            mouseEvents();
+            deleteEvents();
+        }
+    });
+}
